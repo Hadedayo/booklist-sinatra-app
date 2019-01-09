b6b7898bbe58b063585d791b5c548fd30a5c958c
Your Name <you@example.com>
2019-01-09 12:12:32 +0000

    Tesing the keys and values returned from the single book api

--- lib/services/single_books.rb
+++ lib/services/single_books.rb
@@ -6,15 +6,45 @@ class SingleBookService
 
   base_uri 'http://localhost:9292'
 
+  attr_reader :book_title, :author, :age, :gender
+
+  def initialize
+    @book_title = "The Hate U Give"
+    @author = 'Angie Thomas'
+    @age = 34.0
+    @gender = 'Female'
+
+  end
+
   def get_single_book
     @single_book_data = JSON.parse(self.class.get("/api/single-book").body)
+    @single_book_data[0]
+  end
+
+  def get_id
+    @single_book_data[0]["_id"]
+  end
+
+  def get_oid_character
+    @single_book_data[0]["_id"]["$oid"]
   end
 
   def get_title
-    @single_book_data
+    @single_book_data[0]["title"]
+  end
+
+  def get_author
+    @single_book_data[0]["author"]
+  end
+
+  def get_age
+    @single_book_data[0]["age"]
+  end
+
+  def get_gender
+    @single_book_data[0]["gender"]
   end
 
 end
 test = SingleBookService.new
 puts test.get_single_book
-puts test.get_single_book.class
--- spec/single_books_service_spec.rb
+++ spec/single_books_service_spec.rb
@@ -1,14 +1,16 @@
 describe SingleBookService do
 
   before(:all) do
-    @single_book_service = SingleBookService.new.get_single_book
+    @single_book_service = SingleBookService.new
   end
 
   it "should have a results hash" do
-    expect(@single_book_service).to be_kind_of Hash
+    expect(@single_book_service.get_single_book).to be_kind_of Hash
+  end
+
+  it "should return an oid character" do
+    expect(@single_book_service.get_oid_character).to be_kind_of String
   end
 
   
-
-
 end
